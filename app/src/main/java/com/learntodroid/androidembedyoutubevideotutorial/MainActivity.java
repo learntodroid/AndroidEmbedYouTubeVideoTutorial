296eb27c62982987a54f0e9f975f0ac4e98f126c
learntodroid <56079350+learntodroid@users.noreply.github.com>
2020-08-23 12:07:23 +1000

    Initial commit

---
+++ app/src/main/java/com/learntodroid/androidembedyoutubevideotutorial/MainActivity.java
@@ -0,0 +1,31 @@
+package com.learntodroid.androidembedyoutubevideotutorial;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+    private YouTubePlayerView youTubePlayerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        youTubePlayerView = findViewById(R.id.activity_main_youtubePlayerView);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = "1FJHYqE0RDg";
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+        });
+    }
+}
